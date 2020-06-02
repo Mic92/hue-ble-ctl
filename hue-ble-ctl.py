335fe388566baef2daceed11e455a7b46aaf6c2a
Jörg Thalheim <joerg@thalheim.io>
2020-06-02 09:49:42 +0100

    hue-ble-ctl: make cli non-blocking

--- hue-ble-ctl.py
+++ hue-ble-ctl.py
@@ -1,21 +1,40 @@
 #!/usr/bin/env python
-import math
 import sys
-import time
 
 import dbus
 import gatt
+import struct
+from typing import List
+from threading import Thread, Barrier
 
 LIGHT_CHARACTERISTIC = "932c32bd-0002-47a2-835a-a8d455b859dd"
 BRIGHTNESS_CHARACTERISTIC = "932c32bd-0003-47a2-835a-a8d455b859dd"
-COLOR_CHARACTERISTIC = "932c32bd-0005-47a2-835a-a8d455b859dd"
+COLOR_CHARACTERISTIC = "932c32bd-0004-47a2-835a-a8d455b859dd"
+
+
+
+#def convert_xyz_to_xy(r: int, g: int, b: int):
+
+#    """
+#    Returns xy values in the CIE 1931 colorspace after a RGB to XYZ conversion using Wide RGB D65 conversion formula been applied.
+#    """
+#    X = r * 0.649926 + g * 0.103455 + b * 0.197109
+#    Y = r * 0.234327 + g * 0.743075 + b * 0.022598
+#    Z = r * 0.0000000 + g * 0.053077 + b * 1.035763
+#
+#    x = round(X / (X + Y + Z), 4)
+#    y = round(Y / (X + Y + Z), 4)
+#    return x, y
 
 
 class HueLight(gatt.Device):
     def __init__(
-        self, action: str, mac_address: str, manager: gatt.DeviceManager
+        self, action: str, extra_args: List[str], mac_address: str, manager: gatt.DeviceManager,
+        barrier: Barrier
     ) -> None:
         self.action = action
+        self.extra_args = extra_args
+        self.barrier = barrier
 
         print(f"connect to {mac_address}...")
         super(HueLight, self).__init__(mac_address=mac_address, manager=manager)
@@ -34,6 +53,13 @@ class HueLight(gatt.Device):
                     except UnicodeDecodeError:
                         val = ary
                 print(f"  characteristic: {c.uuid}: {val}")
+
+    def set_color(self) -> None:
+        foo = self.color.write_value(struct.pack("i", 1000))
+
+    def set_brightness(self, val: int) -> None:
+        self.brightness.write_value(struct.pack("B", val))
+        print(self.brightness.read_value())
 
     def toggle_light(self) -> None:
         val = self.light_state.read_value()
@@ -65,10 +91,14 @@ class HueLight(gatt.Device):
             self.toggle_light()
         elif self.action == "introspect":
             self.introspect()
+        elif self.action == "color":
+            self.set_color()
+        elif self.action == "brightness":
+            self.set_brightness(int(self.extra_args[0]))
         else:
             print(f"unknown action {self.action}")
             sys.exit(1)
-        sys.exit(0)
+        self.barrier.wait()
 
 
 # shannans_lamp = "cd:43:95:fe:ce:d6"
@@ -77,16 +107,23 @@ class HueLight(gatt.Device):
 
 def main():
     if len(sys.argv) < 3:
-        print(f"USAGE: {sys.argv[0]} toggle|introspect macaddress", file=sys.stderr)
+        print(f"USAGE: {sys.argv[0]} toggle|brightness|introspect macaddress args...", file=sys.stderr)
         sys.exit(1)
 
     mac_address = sys.argv[2]
     # FIXME adapter_name should be configurable
     manager = gatt.DeviceManager(adapter_name="hci0")
-    device = HueLight(sys.argv[1], mac_address=mac_address, manager=manager)
-    device.connect()
+    # this is a bit of a hack. gatt blocks indefinitely
+    b = Barrier(2)
+    device = HueLight(sys.argv[1], sys.argv[3:], mac_address=mac_address, manager=manager, barrier=b)
+    def run():
+        device.connect()
+        manager.run()
+    t = Thread(target=run, daemon=True)
+    t.start()
+    b.wait()
 
-    manager.run()
+
 
 
 if __name__ == "__main__":
